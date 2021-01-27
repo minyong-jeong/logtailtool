6c4fda0446b5f9be13e778c675f89e6f60ca88aa
Minyong Jeong <jmy3155@gmail.com>
2021-01-27 21:22:42 +0900

    Add logtailtool:0.0.1

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# logtailtool
+# LogTailTool
+
+LogTailTool tail a log file in python.
+
+## Usage
+
+```python
+from logtailtool import LogTailTool
+
+lines = LogTailTool("name.log")
+for line in lines:
+    print(line)
+```
---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="logtailtool",
+    version="0.0.1",
+    license='MIT',
+    author="minyong-jeong",
+    author_email="jmy3155@gmail.com",
+    description="LogTailTool tail a log file in python",
+    url="https://github.com/minyong-jeong/logtailtool",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
