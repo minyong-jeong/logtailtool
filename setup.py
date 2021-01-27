112264e7847b8fc8cf1b70f81ae9b1f6abaaf9ac
Minyong Jeong <jmy3155@gmail.com>
2021-01-27 21:34:35 +0900

    Change version (0.0.1 -> 1.0.0)

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 import setuptools
+from os import path
+
+current_dir = path.abspath(path.dirname(__file__))
+with open(path.join(current_dir, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setuptools.setup(
     name="logtailtool",
-    version="0.0.1",
+    version="1.0.0",
     license='MIT',
     author="minyong-jeong",
     author_email="jmy3155@gmail.com",
     description="LogTailTool tail a log file in python",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url="https://github.com/minyong-jeong/logtailtool",
     packages=setuptools.find_packages(),
     classifiers=[
